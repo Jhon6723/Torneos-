8cae050927868c7a5e4a979dfa72a775b5f6dc41
Jhon <jhon.rivera@jala.university>
2025-01-22 21:34:47 -0500

    Arreglando problemas de ejecucion

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,23 @@
-public class Main {
+import Presentation.controllers.MainViewController;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args); // Llama al método JavaFX para inicializar la app
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("javafx/view/MainView.fxml"));
+        Scene scene = new Scene(fxmlLoader.load()); // Ajusta el tamaño según necesites
+        stage.setTitle("Aplicación de Torneos");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
+
